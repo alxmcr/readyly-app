8fca04b3c7322b4c3e34adf6f5228f2cff319ae3
Alejandro Coca <alxmcr@outlook.com>
2022-12-21 23:58:06 -0400

    stable

--- src/components/TaskListSection/index.tsx
+++ src/components/TaskListSection/index.tsx
@@ -1,11 +1,12 @@
 import useTasks from "../../hooks/useTasks";
 import TaskList from "../TaskList";
+import "./TaskListSection.scss";
 
 export default function TaskListSection() {
   const { tasks } = useTasks();
 
   return (
-    <section>
+    <section className="task-section">
       <TaskList tasks={tasks} />
     </section>
   );
