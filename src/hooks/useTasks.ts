132a57e544d4ff0f5930c4d7213b3a0bab1bab10
Alejandro Coca <alxmcr@outlook.com>
2022-12-23 00:38:07 -0400

    stable

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -23,6 +23,48 @@ export default function useTasks() {
           ta_id: 3,
           ta_description: "Watch TV",
           ta_is_completed: false,
+        },{
+          ta_id: 4,
+          ta_description: "Do homework",
+          ta_is_completed: false,
+        },
+        {
+          ta_id: 5,
+          ta_description: "Go to the doctor",
+          ta_is_completed: true,
+        },
+        {
+          ta_id: 6,
+          ta_description: "Watch TV",
+          ta_is_completed: false,
+        },{
+          ta_id: 7,
+          ta_description: "Do homework",
+          ta_is_completed: false,
+        },
+        {
+          ta_id: 8,
+          ta_description: "Go to the doctor",
+          ta_is_completed: true,
+        },
+        {
+          ta_id: 9,
+          ta_description: "Watch TV",
+          ta_is_completed: false,
+        },{
+          ta_id: 10,
+          ta_description: "Do homework",
+          ta_is_completed: false,
+        },
+        {
+          ta_id: 11,
+          ta_description: "Go to the doctor",
+          ta_is_completed: true,
+        },
+        {
+          ta_id: 12,
+          ta_description: "Watch TV",
+          ta_is_completed: false,
         },
       ];
       setTasks(json);
