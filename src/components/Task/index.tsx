37e712471c775acffbbe29bb8a8b7f7affdda836
Alejandro Coca <alxmcr@outlook.com>
2022-12-23 00:55:57 -0400

    task improvements

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,21 +1,27 @@
 import { ITaskProps } from "../../@types/appTypes";
 import "./Task.scss";
 
-
 export default function Task({ task }: ITaskProps) {
   const { ta_id, ta_description, ta_is_completed } = task;
 
-  if (ta_is_completed) {
-    return (
-      <li id={ta_id.toString()} className="task task--completed">
-        <span className="task__text">{ta_description}</span>
-      </li>
-    );
-  }
-
   return (
-    <li id={ta_id.toString()} className="task">
-      <span className="task__text">{ta_description}</span>
+    <li
+      id={ta_id.toString()}
+      className={ta_is_completed ? `task task--completed` : `task`}
+    >
+      <input
+        type="checkbox"
+        name={`task__description-${ta_id}`}
+        id={`task__description-${ta_id}`}
+        className="task__status"
+        checked={ta_is_completed}
+      />
+      <label
+        htmlFor={`task__description-${ta_id}`}
+        className="task__description"
+      >
+        {ta_description}
+      </label>
     </li>
   );
 }
