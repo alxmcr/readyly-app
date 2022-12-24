bdc9f01ac0e27bacca1952ec3dc587c525876337
Alejandro Coca <alxmcr@outlook.com>
2022-12-24 01:35:35 -0400

    stable

--- src/providers/TasksProvider.tsx
+++ src/providers/TasksProvider.tsx
@@ -1,8 +1,13 @@
-import React from "react";
-import { ReactFCProps } from "../@types/appTypes";
+import React, { useEffect } from "react";
+import { ITask, ReactFCProps } from "../@types/appTypes";
 import { ITasksState, ITasksStateContext } from "../@types/storeTypes";
+import useTasks from "../hooks/useTasks";
 import { TaskService } from "../services/TaskService";
 import { tasksReducer } from "../store/tasks/reducers";
+
+function init(initialTasks: ITask[]) {
+  return { tasks: [...initialTasks] };
+}
 
 const tasksContextInitialState: ITasksStateContext = {
   state: {
@@ -23,17 +28,35 @@ const initializerTasksReducer = async (): Promise<ITasksState> => {
 export const TasksContext = React.createContext(tasksContextInitialState);
 
 export default function TasksProvider({ children }: ReactFCProps) {
-  const tasksInit: ITasksState = { tasks: [] };
-  const [state, dispatch] = React.useReducer(
-    tasksReducer,
-    tasksInit,
-    initializerTasksReducer
-  );
+  const [tasks, setTasks] = React.useState<ITask[]>([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const [state, dispatch] = React.useReducer(tasksReducer, tasks, init);
 
   const value = {
     state,
     dispatch,
+    isLoading,
   };
+
+  useEffect(() => {
+    let ignore = false;
+
+    if (!ignore) {
+      const getTasks = async () => {
+        setIsLoading(true);
+        const tasksService = new TaskService();
+        const tasksResult = await tasksService.fetchTasks();
+        setTasks(tasksResult);
+        setIsLoading(false);
+      };
+
+      getTasks();
+    }
+    return () => {
+      ignore = true;
+    };
+  }, []);
 
   return (
     <TasksContext.Provider value={value}>{children}</TasksContext.Provider>
--- src/store/tasks/reducers.ts
+++ src/store/tasks/reducers.ts
@@ -1,28 +1,7 @@
-import { Reducer } from "react";
 import { ITasksState, ITasksAction } from "../../@types/storeTypes";
 import { TASKS_ACTIONS } from "../../helpers/contants";
-import { TaskService } from "../../services/TaskService";
 
-let initialTasks: ITasksState = {
-  tasks: [],
-};
-const initializerTasksReducer = async () => {
-  const tasksService = new TaskService();
-  const tasksResult = await tasksService.fetchTasks();
-
-  console.log({ tasksResult });
-  initialTasks = {
-    tasks: tasksResult,
-  };
-};
-
-initializerTasksReducer();
-console.log({ initialTasks });
-
-export const tasksReducer: Reducer<ITasksState, ITasksAction> = (
-  state = initialTasks,
-  action
-) => {
+export const tasksReducer = (state: ITasksState, action: ITasksAction) => {
   switch (action?.type) {
     case TASKS_ACTIONS?.ADD:
       return { ...state, tasks: [...state.tasks, action?.payload] };
