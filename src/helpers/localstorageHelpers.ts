c482edd4d862ded1e90c8f377efdaa1a9b0563b0
Alejandro Coca <alxmcr@outlook.com>
2023-01-07 21:42:53 -0400

    improvements

--- src/helpers/localstorageHelpers.ts
+++ src/helpers/localstorageHelpers.ts
@@ -1,3 +1,11 @@
+import { ITask } from '../@types/appTypes';
+
 export const convertLocalStorageToTasks = (tasksAsStr = '[]') => {
   return Array.from(tasksAsStr);
+};
+
+export const getTasksFromLocalstorage = (key = ''): ITask[] => {
+  const tasksStr = window.localStorage.getItem(key);
+
+  return tasksStr ? JSON.parse(tasksStr) : [];
 };
