54af98f0c9b5e2bd50672f49b2587ba38599f344
Alejandro Coca <alxmcr@outlook.com>
2023-01-07 20:39:59 -0400

    improvements

---
+++ src/helpers/localstorageHelpers.ts
@@ -0,0 +1,3 @@
+export const convertLocalStorageToTasks = (tasksAsStr = "[]") => {
+  return Array.from(tasksAsStr);
+};
--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -9,62 +9,62 @@ export class TaskService implements ITaskService {
       setTimeout(() => {
         tasks = [
           {
-            ta_id: 1,
+            ta_id: "1",
             ta_description: "Do homework",
             ta_is_completed: false,
           },
           {
-            ta_id: 2,
+            ta_id: "2",
             ta_description: "Go to the doctor",
             ta_is_completed: true,
           },
           {
-            ta_id: 3,
+            ta_id: "3",
             ta_description: "Watch TV",
             ta_is_completed: false,
           },
           {
-            ta_id: 4,
+            ta_id: "4",
             ta_description: "Do homework",
             ta_is_completed: false,
           },
           {
-            ta_id: 5,
+            ta_id: "5",
             ta_description: "Go to the doctor",
             ta_is_completed: true,
           },
           {
-            ta_id: 6,
+            ta_id: "6",
             ta_description: "Watch TV",
             ta_is_completed: false,
           },
           {
-            ta_id: 7,
+            ta_id: "7",
             ta_description: "Do homework",
             ta_is_completed: false,
           },
           {
-            ta_id: 8,
+            ta_id: "8",
             ta_description: "Go to the doctor",
             ta_is_completed: true,
           },
           {
-            ta_id: 9,
+            ta_id: "9",
             ta_description: "Watch TV",
             ta_is_completed: false,
           },
           {
-            ta_id: 10,
+            ta_id: "10",
             ta_description: "Do homework",
             ta_is_completed: false,
           },
           {
-            ta_id: 11,
+            ta_id: "11",
             ta_description: "Go to the doctor",
             ta_is_completed: true,
           },
           {
-            ta_id: 12,
+            ta_id: "12",
             ta_description: "Watch TV",
             ta_is_completed: false,
           },
