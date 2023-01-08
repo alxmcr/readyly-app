0f90456cd806b1af15ae773f2efe4b0547ee3d73
Alejandro Coca <alxmcr@outlook.com>
2023-01-07 21:45:44 -0400

    stable

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { ITasksState } from './@types/storeTypes';
-import { TASKS_KEY_ITEM } from './helpers/contants';
-import useTasksLocalStorage from './hooks/useTasksLocalStorage';
+import { getTasksFromLocalstorage } from './helpers/localstorageHelpers';
 import HomePage from './pages/HomePage';
 import TasksProvider from './providers/TasksProvider';
 
 export default function App() {
-  const tasksState: ITasksState = useTasksLocalStorage(TASKS_KEY_ITEM);
-
-  if (tasksState?.isLoading) {
-    return <p>Loading...</p>;
-  }
+  const tasks = getTasksFromLocalstorage();
 
   return (
-    <TasksProvider tasksState={tasksState}>
+    <TasksProvider tasksState={{ isLoading: false, tasks }}>
       <div className="App">
         <HomePage />
       </div>
--- src/helpers/localstorageHelpers.ts
+++ src/helpers/localstorageHelpers.ts
@@ -1,11 +1,12 @@
 import { ITask } from '../@types/appTypes';
+import { TASKS_KEY_ITEM } from './contants';
 
 export const convertLocalStorageToTasks = (tasksAsStr = '[]') => {
   return Array.from(tasksAsStr);
 };
 
-export const getTasksFromLocalstorage = (key = ''): ITask[] => {
-  const tasksStr = window.localStorage.getItem(key);
+export const getTasksFromLocalstorage = (): ITask[] => {
+  const tasksStr = window.localStorage.getItem(TASKS_KEY_ITEM);
 
   return tasksStr ? JSON.parse(tasksStr) : [];
 };
